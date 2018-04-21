da6b222c83b59be9952e92d583df6b4f5b226659
Jay Pitman <jaypitman@Jays-MBP.gateway>
2018-04-21 20:05:34 +1000

    add homepage to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'home/index'
+
+  get 'home/contact'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
